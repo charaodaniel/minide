d2ea4caa4fe0e9cd56b101bf209d35e2832ce303
Daniel <47531147+charaodaniel@users.noreply.github.com>
2026-03-14 23:24:02 -0300

    teste init

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+while true; do
+clear
+echo "╔══════════════════╗"
+echo "║      MinIDE      ║"
+echo "╚══════════════════╝"
+echo ""
+echo "[1] CLI Editor"
+echo "[2] GUI Editor"
+echo "[0] Exit"
+echo ""
+
+read -p "Select option: " opt
+
+case $opt in
+1)
+cd cli && cargo run
+break
+;;
+2)
+cd gui && npm run tauri dev
+break
+;;
+0)
+exit
+;;
+*)
+echo "Invalid option"
+sleep 1
+;;
+esac
+done
